eca2ada0b8c088723fe54da563a85daca3b96c45
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:45:07 -0600

    v1.2.0

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -1,3 +1,6 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array
+
+/**
+ * Extract of translatable text strings from the static config array.
+ */
 _x( 'WPSSO Core add-on to provide an array of meta tags and Schema markup in the WordPress REST API post, term and user queries.', 'plugin description', 'wpsso-rest-api' );
