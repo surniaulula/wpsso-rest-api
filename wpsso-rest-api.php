3ba39d48d246895206d6204439f31d234fa09ba0
JS Morisset <jsm@surniaulula.com>
2019-07-31 21:27:07 -0700

    v2.0.0

--- wpsso-rest-api.php
+++ wpsso-rest-api.php
@@ -220,7 +220,7 @@ if ( ! class_exists( 'WpssoRest' ) ) {
 			}
 
 			if ( ! $this->have_req_min ) {
-				return;	// stop here
+				return;	// Stop here.
 			}
 
 			$this->filters = new WpssoRestFilters( $this->p );
@@ -236,7 +236,7 @@ if ( ! class_exists( 'WpssoRest' ) ) {
 
 				$this->min_version_notice();
 
-				return;	// stop here
+				return;	// Stop here.
 			}
 		}
 
