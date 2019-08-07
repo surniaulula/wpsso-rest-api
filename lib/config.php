4734b3d93d93c6ecbf659b5605f4820e10ae242e
JS Morisset <jsm@surniaulula.com>
2019-08-07 07:18:15 -0700

    v2.0.1-rc.1

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoRestConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssorest' => array(			// Plugin acronym.
-					'version'     => '2.0.1-dev.3',	// Plugin version.
+					'version'     => '2.0.1-rc.1',	// Plugin version.
 					'opt_version' => '1',		// Increment when changing default option values.
 					'short'       => 'WPSSO REST',	// Short plugin name.
 					'name'        => 'WPSSO REST API',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoRestConfig' ) ) {
 					'req'         => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '5.5.1-dev.3',
+						'min_version' => '5.5.1-rc.1',
 					),
 					'assets' => array(
 						'icons' => array(
