d133a6d551cb269cc03c19c7163d7fc2a75ed2ed
JS Morisset <jsm@surniaulula.com>
2020-09-13 10:53:41 -0700

    v2.6.1

--- lib/gettext-config.php
+++
@@ -1,6 +0,0 @@
-<?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable text strings from the static config array.
- */
-_x( 'Enhances the WordPress REST API Post, Term and User Queries with an Array of Social Meta Tags, SEO HTML Tags and Schema JSON-LD Markup.', 'plugin description', 'wpsso-rest-api' );
