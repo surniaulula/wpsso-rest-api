d7fa82bdebd995bf8a012ac64e4bf5a004123b1e
JS Morisset <jsm@surniaulula.com>
2020-09-13 11:03:35 -0700

    v2.6.1

---
+++ gettext/gettext-config.php
@@ -0,0 +1,6 @@
+<?php die( 'These aren\'t the droids you\'re looking for.' );
+
+/**
+ * Extract of translatable strings from the config array.
+ */
+_x( 'Enhances the WordPress REST API Post, Term and User Queries with an Array of Social Meta Tags, SEO HTML Tags and Schema JSON-LD Markup.', 'plugin description', 'wpsso-rest-api' );
