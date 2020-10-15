70ccd21e8bac77b780fe6fe2b69f75f317026831
JS Morisset <jsm@surniaulula.com>
2020-10-15 14:38:37 -0700

    v2.7.0-b.1

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -4,4 +4,4 @@
  * Extract of translatable strings from lib/config.php.
  */
 _x( 'WPSSO REST API', 'plugin name', 'wpsso-rest-api' );
-_x( 'Enhances the WordPress REST API Post, Term and User Queries with an Array of Social Meta Tags, SEO HTML Tags and Schema JSON-LD Markup.', 'plugin description', 'wpsso-rest-api' );
+_x( 'Enhances the WordPress REST API post, term and user queries with an array of social meta tags, SEO HTML tags and Schema JSON-LD markup.', 'plugin description', 'wpsso-rest-api' );
