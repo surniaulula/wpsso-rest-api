84ace475151b4a1f11b4146773fada4657ffc27f
JS Morisset <jsm@surniaulula.com>
2021-06-25 09:46:00 -0700

    v2.8.1

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -4,4 +4,4 @@
  * Extract of translatable strings from lib/config.php.
  */
 _x( 'WPSSO REST API', 'plugin name', 'wpsso-rest-api' );
-_x( 'Enhances the WordPress REST API post, term and user queries with an array of social meta tags, SEO HTML tags and Schema JSON-LD markup.', 'plugin description', 'wpsso-rest-api' );
+_x( 'Enhances the WordPress REST API post, term, and user queries with an array of meta tags and Schema JSON-LD markup.', 'plugin description', 'wpsso-rest-api' );
--- lib/config.php
+++ lib/config.php
@@ -21,7 +21,7 @@ if ( ! class_exists( 'WpssoRestConfig' ) ) {
 					'opt_version' => '1',		// Increment when changing default option values.
 					'short'       => 'WPSSO REST',	// Short plugin name.
 					'name'        => 'WPSSO REST API',
-					'desc'        => 'Enhances the WordPress REST API post, term and user queries with an array of social meta tags, SEO HTML tags and Schema JSON-LD markup.',
+					'desc'        => 'Enhances the WordPress REST API post, term, and user queries with an array of meta tags and Schema JSON-LD markup.',
 					'slug'        => 'wpsso-rest-api',
 					'base'        => 'wpsso-rest-api/wpsso-rest-api.php',
 					'update_auth' => '',		// No premium version.
