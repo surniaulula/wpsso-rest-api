1dd8c273852373804151792e08f547135d221d71
JS Morisset <jsm@surniaulula.com>
2021-07-09 13:53:06 -0700

    v2.8.1

--- wpsso-rest-api.php
+++ wpsso-rest-api.php
@@ -13,7 +13,7 @@
  * Description: Enhances the WordPress REST API post, term, and user query results with an array of meta tags and Schema JSON-LD markup.
  * Requires PHP: 7.0
  * Requires At Least: 4.7
- * Tested Up To: 5.7.2
+ * Tested Up To: 5.8
  * Version: 2.8.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
