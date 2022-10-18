074d7224d668254941cd40f7cbcfa6dd2853b628
JS Morisset <jsm@surniaulula.com>
2022-10-18 09:34:56 -0700

    v2.10.1

--- wpsso-rest-api.php
+++ wpsso-rest-api.php
@@ -11,6 +11,7 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Extends the WordPress REST API post, term, and user query results with an array of meta tags and Schema JSON-LD markup.
+ * Requires Plugins: wpsso
  * Requires PHP: 7.2
  * Requires At Least: 5.2
  * Tested Up To: 6.1.0
