a8797a102bb0d8e5e7ed817ec169ce8923a0b0a6
JS Morisset <jsm@surniaulula.com>
2022-12-28 12:06:23 -0800

    v2.10.1

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,8 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable strings from lib/config.php.
- */
+// 
+// Extract of translatable strings from lib/config.php.
+// 
 _x( 'WPSSO REST API', 'plugin name', 'wpsso-rest-api' );
 _x( 'Extends the WordPress REST API post, term, and user query results with an array of meta tags and Schema JSON-LD markup.', 'plugin description', 'wpsso-rest-api' );
 _x( 'WPSSO Core', 'plugin name', 'wpsso-rest-api' );
